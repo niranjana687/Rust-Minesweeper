87c3f629328b56137d2e4c2f68e93d9331e9e6e4
niranjana687 <niranjana687@gmail.com>
2020-09-08 12:08:23 +0530

    added player and real board

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::time::Duration;
 
 const BEGINNER :i32 = 0;
 const INTERMEDIATE :i32 = 1;
@@ -6,11 +7,27 @@ const ADVANCED :i32 = 2;
 const MAXSIDE :i32 = 25;
 const MAXMINES :i32 = 99;
 const MOVESIZE :i32 = 526;
+//stroes player info
+pub struct Player {
+    name: String,
+    age: u8,
+    time: Duration,
+    //wins in one game = 1 point
+    score: i32,
+}
 
 pub struct MineGame {
     side :i32,
     mines :i32,
+    player: Player,
 } 
+
+pub struct RealBoard{
+    side: i32,
+    mines: i32,
+}
+
+
 
 impl MineGame {
     pub fn is_valid(&self, row: i32, col :i32) -> bool {
@@ -20,6 +37,11 @@ impl MineGame {
             return false;
         }
     }
+
+    pub fn choose_difficulty(level: String) -> MineGame {
+
+    }
+    pub fn cheat_minesweeper() ->
 }
 
 
