dad6be45a50f957f72e0849a08fef300db3710cb
niranjana687 <niranjana687@gmail.com>
2020-09-10 00:33:10 +0530

    game field print

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,6 @@
 pub mod minesweepr {
 
-    struct MineSweeper {
-
-    }
-
-    struct Cell {
-
-    }
-
-    enum CellContent {
-
-    }
-
-    enum Mark {
-
-    }
-
-    pub enum GameStatus {
+    pub struct MineSweeper {
         
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #[derive(Debug, Copy, Clone)]
 
 pub struct MineGame {
-    width: i32,
-    height: i32,
+    row: i32,
+    col: i32,
     mines: i32,
+
+}
+
+pub struct Game  {
 
 }
 
@@ -19,32 +23,53 @@ const BEGINNER :MineGame = MineGame {
     height: 9,
     mines: 10,
 };
-const INTERMEDIATE :MineGame = MineGame {
-    width: 16,
-    height: 16,
-    mines: 40,
-};
+// const INTERMEDIATE :MineGame = MineGame {
+//     width: 16,
+//     height: 16,
+//     mines: 40,
+// };
 
-const ADVANCED :MineGame = MineGame {
-    width: 24,
-    height: 24,
-    mines: 99,
-};
+// const ADVANCED :MineGame = MineGame {
+//     width: 24,
+//     height: 24,
+//     mines: 99,
+// };
 
 impl MineGame {
     
-    fn game_level(level: i8) -> MineGame {
-        match level {
-            0 => BEGINNER,
-            1 => INTERMEDIATE,
-            2 => ADVANCED,
-            _ => panic!("Invalid input. 
-            Try 0-> Beginner
-                1-> Intermediate
-                2-> Advanced"),
-        }
+    // fn game_level(level: i8) -> MineGame {
+    //     match level {
+    //         0 => BEGINNER,
+    //         1 => INTERMEDIATE,
+    //         2 => ADVANCED,
+    //         _ => panic!("Invalid input. 
+    //         Try 0-> Beginner
+    //             1-> Intermediate
+    //             2-> Advanced"),
+    //     }
 
+    // }
+
+    pub fn print(&self) {
+        print!("┌");
+        for _ in 0..self.col{
+            print!("─");
+        }
+        println!("┐");
+        for y in 0..self.row {
+            print!("│");
+            for x in 0..self.col {
+                
+    
+            println!("│");
+        }
+        print!("└");
+        for _ in 0..self.col {
+            print!("─");
+        }
+        println!("┘");
     }
+    
 
 
 }
@@ -54,18 +79,20 @@ pub struct PlayBoard {
 }
 
 fn main() {
- println!(" \tM I N E S W E E P E E R\n
-            \n
-            COMMANDS: 
-            \n
-            row col -> reveal row, col\n
-            m row col-> mark row, col\n
-            q-> quit\n
-            n-> new game\n
-            b-> Beginner\n
-            i-> Intermediate\n
-            a-> Advanced\n
+ loop {
+    println!(" \tM I N E S W E E P E R\n
+    \n
+    COMMANDS: 
+    \n
+    row col -> reveal row, col\n
+    m row col-> mark row, col\n
+    q-> quit\n
+    n-> new game\n
+    b-> Beginner\n
+    i-> Intermediate\n
+    a-> Advanced\n
 
-
- ");
+   
+");
+ }
 }
