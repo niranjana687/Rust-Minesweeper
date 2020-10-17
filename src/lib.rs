0e78a2e816fbf51cc00dc9118541eb22726d9a66
Niranjana <niranjana687@gmail.com>
2020-10-17 23:06:45 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,12 @@ pub mod Minesweeper {
     pub struct GameBoard{
         rows: u8,
         cols: u8,
-        mines: u8,
+        mines: Mine,
 
+    }
+    
+    pub struct Mine {
+    mine: bool
     }
 
     
